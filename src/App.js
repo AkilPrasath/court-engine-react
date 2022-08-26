fac145268a9042e865549cae557ec89adf252406
Akil Prasath R <akilprasathr@gmail.com>
2022-08-26 15:41:30 +0530

    no error build

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import SignInOutContainer from './containers';
-import Home from './Components/home'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInOutContainer from "./containers";
+import Home from "./Components/home";
+import PdfView from "./Components/pdf_view";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SignInOutContainer />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-    // <div className="App">
-    //  <SignInOutContainer/>
-    // </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SignInOutContainer />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/pdf" element={<PdfView />} />
+			</Routes>
+		</BrowserRouter>
+		// <div className="App">
+		//  <SignInOutContainer/>
+		// </div>
+	);
 }
 
 export default App;
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,9 +1,92 @@
-import { Grid } from "@mui/material";
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	Grid,
+	Menu,
+	MenuItem,
+	TextField,
+	Typography,
+} from "@mui/material";
+import MicIcon from "@mui/icons-material/Mic";
+import MicOffIcon from "@mui/icons-material/MicOff";
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
+import { useState } from "react";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import "./home.css";
 import Navbar from "./Navbar";
+import auth from "../firebase_config.js";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import SpeechRecognition, {
+	useSpeechRecognition,
+} from "react-speech-recognition";
+import { useEffect } from "react";
+
+// function search(dataMap) {
+// 	console.log(dataMap);
+// 	dataMap["queryText"] = "query";
+// 	dataMap["text_sections"] = [];
+// 	const requestOptions = {
+// 		method: "POST",
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 			"Access-Control-Allow-Origin": "*",
+// 		},
+// 		body: JSON.stringify(dataMap),
+// 	};
+// 	fetch("http://127.0.0.1:8000/search", requestOptions)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 		});
+// }
+
 function Home() {
+	const [queryText, setQueryText] = useState("");
+	const {
+		transcript,
+		listening,
+		resetTranscript,
+		browserSupportsSpeechRecognition,
+	} = useSpeechRecognition();
+	useEffect(() => {
+		setQueryText(transcript);
+	}, [transcript]);
+
+	const [isMicOn, setMicStatus] = useState(false);
+	const navigate = useNavigate();
+	const [results, setResults] = useState([]);
+	const [filterMap, setFilterMap] = useState({
+		petitioner: [],
+		respondent: [],
+		section: [],
+		court: "All",
+		highCourtLocation: "",
+		judgementNumber: "",
+	});
+	const search = (dataMap) => {
+		console.log(dataMap);
+		dataMap["queryText"] = queryText;
+		dataMap["text_sections"] = [];
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "*",
+			},
+			body: JSON.stringify(dataMap),
+		};
+		fetch("http://127.0.0.1:8000/search", requestOptions)
+			.then((response) => response.json())
+			.then((data) => {
+				var li = data["hits"]["hits"];
+				console.log(data);
+				setResults(li);
+			});
+	};
 	const items = [
 		{
 			id: 0,
@@ -55,36 +138,209 @@ function Home() {
 			</div>
 		);
 	};
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<div className="App" style={{ height: "100%", width: "100%" }}>
+			<Grid container columns={12}>
+				<Grid item md={11}></Grid>
+				<Grid item md={1}>
+					<div>
+						<Button
+							id="basic-button"
+							aria-controls={open ? "basic-menu" : undefined}
+							aria-haspopup="true"
+							aria-expanded={open ? "true" : undefined}
+							onClick={handleClick}>
+							Account
+						</Button>
+						<Menu
+							id="basic-menu"
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							MenuListProps={{
+								"aria-labelledby": "basic-button",
+							}}>
+							<MenuItem onClick={handleClose}>Profile</MenuItem>
+							<MenuItem onClick={handleClose}>
+								My account
+							</MenuItem>
+							<MenuItem
+								onClick={() => {
+									signOut(auth);
+									navigate("/", { replace: "true" });
+								}}>
+								Logout
+							</MenuItem>
+						</Menu>
+					</div>
+				</Grid>
+			</Grid>
 			<Grid
 				container
 				spacing={2}
 				columns={12}
 				style={{ padding: "24px" }}>
 				<Grid item md={3}>
-					<Navbar />
+					<Navbar
+						setMap={setFilterMap}
+						filterMap={filterMap}
+						callSearchApi={() => {
+							search(filterMap);
+						}}
+					/>
 				</Grid>
+				<Grid item md={1}></Grid>
 				<Grid item md={5}>
-					<div style={{ marginTop: "30vh" }}>
+					<div style={{ marginTop: "5vh" }}>
 						<h1 style={{ textAlign: "center", fontSize: "3rem" }}>
-							J Search
+							<Typography variant="h2" component="h3">
+								J Search
+							</Typography>
 						</h1>
-						<ReactSearchAutocomplete
-							items={items}
-							onSearch={handleOnSearch}
-							onHover={handleOnHover}
-							onSelect={handleOnSelect}
-							onFocus={handleOnFocus}
-							onClear={handleOnClear}
-							formatResult={formatResult}
-							styling={{ zIndex: 4 }} // To display it on top of the search box below
-							autoFocus
-						/>
+
+						<Grid container justifyContent={"center"}>
+							<Grid item>
+								<TextField
+									inputProps={{
+										style: {
+											padding: 5,
+										},
+									}}
+									style={{ width: "500px" }}
+									value={queryText}
+									onChange={(event) => {
+										setQueryText(event.target.value);
+									}}
+								/>
+							</Grid>
+							<Grid item>
+								<Button
+									onClick={() => {
+										const requestOptions = {
+											method: "POST",
+											headers: {
+												"Content-Type":
+													"application/json",
+												"Access-Control-Allow-Origin":
+													"*",
+											},
+											body: JSON.stringify({
+												queryString: queryText,
+											}),
+										};
+										fetch(
+											"http://127.0.0.1:8000/searchQuery",
+											requestOptions
+										)
+											.then((response) => response.json())
+											.then((data) => {
+												var li = data["hits"]["hits"];
+												console.log(li);
+												setResults(li);
+											});
+									}}>
+									<SearchIcon />
+								</Button>
+							</Grid>
+						</Grid>
+						<Grid container justifyContent={"center"}>
+							<Grid item>
+								<Typography variant="h6">
+									Microphone:{" "}
+									{listening ? "listening" : "not listening"}
+								</Typography>
+							</Grid>
+							<Grid item>
+								<Button
+									onClick={() => {
+										if (!isMicOn) {
+											SpeechRecognition.startListening();
+											setMicStatus(true);
+										} else {
+											SpeechRecognition.stopListening();
+											setMicStatus(false);
+										}
+									}}>
+									{isMicOn && <MicIcon />}
+									{!isMicOn && <MicOffIcon />}
+								</Button>
+							</Grid>
+							<Grid item>
+								<Button
+									onClick={() => {
+										setMicStatus(false);
+										setQueryText("");
+										resetTranscript();
+									}}>
+									Reset
+								</Button>
+							</Grid>
+						</Grid>
+
+						<br />
+						<br />
+						{results == [] && (
+							<h2>
+								No Results Found. Try with different filters
+							</h2>
+						)}
+						{results.map((data, index) => {
+							return (
+								<ResultCard
+									key={index}
+									id={data["_id"]}
+									heading={data["_source"]["queryText"]
+										.split(" ")
+										.splice(0, 10)
+										.join(" ")}
+									subtext={data["_source"]["queryText"]
+										.split(" ")
+										.splice(10, 70)
+										.join(" ")}
+								/>
+							);
+						})}
 					</div>
 				</Grid>
 			</Grid>
 		</div>
+	);
+}
+function ResultCard({ id, heading, subtext, ...props }) {
+	const navigate = useNavigate();
+	return (
+		<Card
+			style={{ marginTop: "20px" }}
+			variant="outlined"
+			onClick={() => {
+				navigate("/pdf?id=" + id);
+			}}>
+			<CardContent>
+				<Typography
+					sx={{ fontSize: 14 }}
+					color="text.secondary"
+					gutterBottom>
+					{id}
+				</Typography>
+				<a href="">
+					{" "}
+					<Typography variant="h5" component="div">
+						{heading}
+					</Typography>
+				</a>
+				<br />
+				<Typography variant="body2">{subtext}</Typography>
+			</CardContent>
+		</Card>
 	);
 }
 
